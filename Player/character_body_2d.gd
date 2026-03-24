604c71f236a726a8a0b60bc3266ca18687adc2e6
Yurii Chayka <yuryychayka09@gmail.com>
2026-03-24 22:17:49 +0200

    A menu has been added

--- Player/character_body_2d.gd
+++ Player/character_body_2d.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var speed: float = 300.0
 @export var jump_velocity: float = -400.0
 
+@onready var sprite: AnimatedSprite2D = $PlayerSprite2D
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -13,8 +15,15 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction != 0:
 		velocity.x = direction * speed
-		$PlayerSprite2D.flip_h = direction < 0
+		sprite.flip_h = direction < 0
+		sprite.play("Walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
+		sprite.stop()  # стоїмо — зупиняємо анімацію
 
 	move_and_slide()
+
+# Додаємо вихід у меню через Esc
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"): # стандартно це Esc
+		get_tree().change_scene_to_file("res://Skripts/control 01.tscn")
